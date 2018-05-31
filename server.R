6fd7557cb0b4133b8cc0a08dd7dd32a644d122b5
Lucy <ld.mcgowan@vanderbilt.edu>
2018-05-31 09:25:49 -0500

    🔃 Switch to website as data source rather than Google Sheets

--- server.R
+++ server.R
@@ -1,25 +1,21 @@
 library(shiny)
 library(scales)
-library(googlesheets)
-library(dplyr)
 library(plotrix)
+library(purrr)
 
-googlesheets::gs_auth(token = "shiny_app_token.rds")
 shinyServer(function(input, output) {
   
-  d <- gs_url("https://docs.google.com/spreadsheets/d/1L_KwDsxh7fj8Djd6KSmZ1r5pbrQGWqfg4ZnS6IXI2xk/edit?usp=sharing") %>%
-    gs_read() 
-  
-  df <- d %>%
-    summarise(total = sum(Amount, na.rm = TRUE)) %>%
-    mutate(funds = "funds") 
+  total <- httr::GET("http://ww2.amstat.org/misc/test.cfm") %>%
+    httr::content(as = "text", encoding = "UTF-8") %>%
+    as.numeric()
   
   output$total <- renderText({
-    dollar(df$total)
+    dollar(total)
   })
   output$remaining <- renderText({
-    dollar(30000 - df$total)
+    dollar(30000 - total)
   })
+  df <- data.frame(total = total)
   output$plot <- renderPlot({
     barplot(df$total, xlim = c(0.1, 5), ylim = c(-10000, 30000), col = 2, border = 2, xaxt = "n", yaxt = "n")
     draw.ellipse(0.7, -2000, 0.6, 6000, col = 2, border = 2)
