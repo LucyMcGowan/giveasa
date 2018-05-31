7e775e62833161c300a485dcf3b77e3a37848c14
Lucy <ld.mcgowan@vanderbilt.edu>
2018-05-31 09:51:28 -0500

    Update plot aspect ratio

--- server.R
+++ server.R
@@ -17,10 +17,21 @@ shinyServer(function(input, output) {
   })
   df <- data.frame(total = total)
   output$plot <- renderPlot({
-    barplot(df$total, xlim = c(0.1, 5), ylim = c(-10000, 30000), col = 2, border = 2, xaxt = "n", yaxt = "n")
+    barplot(
+      df$total,
+      xlim = c(0.1, 1.5),
+      ylim = c(-10000, 30000), 
+      col = 2, 
+      border = 2,
+      xaxt = "n", 
+      yaxt = "n",
+      asp = 1/15000
+      )
     draw.ellipse(0.7, -2000, 0.6, 6000, col = 2, border = 2)
     rect(0.2, df$total, 1.2, 30000, col = "white", border = NA)
-    rect(0.2, 0, 1.2, 30000, border = 2)
-    axis(2, at = seq(0, 30000, by = 5000), las = 2)
+    rect(0.2, 0, 1.2, 30000, border = 2, lwd = 3)
+    axis(2, at = seq(0, 30000, by = 5000), las = 2, lwd = 3,
+         labels = sprintf("$%s", seq(0, 30000, by = 5000))
+    )
   })
 })
--- ui.R
+++ ui.R
@@ -7,12 +7,15 @@ body <- dashboardBody(
         img(src = "http://www.amstat.org/images/ASAImages/giving/GiveASAlogo.png"))
   ),
   fluidRow(
-    box(status = "primary", 
-        plotOutput("plot")
+    box(
+      width = 6,
+      height = 500,
+      status = "primary", 
+      plotOutput("plot")
     ),
-    valueBox(uiOutput("total"), "Total funding", icon("money")),
-    valueBox(uiOutput("remaining"), "Total remaining", icon("money"), color = "red"),
-    infoBox(h4("Click here to donate"), href = "https://ww2.amstat.org/giving/givenow.cfm", color = "purple", icon = icon("thumbs-up"))
+    valueBox(width = 3, uiOutput("total"), "Total funding", icon("money")),
+    valueBox(width = 3, uiOutput("remaining"), "Total remaining", icon("money"), color = "red"),
+    infoBox(width = 6, h3("Click here to donate"), href = "https://ww2.amstat.org/giving/givenow.cfm", color = "purple", icon = icon("thumbs-up"))
   )
 )
 dashboardPage(
