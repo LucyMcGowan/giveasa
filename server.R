ea2614b2c6a61049386f3a5dc9661e070509341f
Lucy <ld.mcgowan@vanderbilt.edu>
2018-06-22 13:35:15 -0400

    Update dependencies

--- server.R
+++ server.R
@@ -1,19 +1,19 @@
 library(shiny)
-library(scales)
-library(plotrix)
-library(purrr)
 
 shinyServer(function(input, output) {
   
-  total <- httr::GET("http://ww2.amstat.org/misc/test.cfm") %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    as.numeric()
+  total <- as.numeric( 
+    httr::content(
+      httr::GET("http://ww2.amstat.org/misc/test.cfm"),
+      as = "text", encoding = "UTF-8"
+    )
+  )
   
   output$total <- renderText({
-    dollar(total)
+    scales::dollar(total)
   })
   output$remaining <- renderText({
-    dollar(30000 - total)
+    scales::dollar(30000 - total)
   })
   df <- data.frame(total = total)
   output$plot <- renderPlot({
@@ -26,8 +26,8 @@ shinyServer(function(input, output) {
       xaxt = "n", 
       yaxt = "n",
       asp = 1/15000
-      )
-    draw.ellipse(0.7, -2000, 0.6, 6000, col = 2, border = 2)
+    )
+    plotrix::draw.ellipse(0.7, -2000, 0.6, 6000, col = 2, border = 2)
     rect(0.2, df$total, 1.2, 30000, col = "white", border = NA)
     rect(0.2, 0, 1.2, 30000, border = 2, lwd = 3)
     axis(2, at = seq(0, 30000, by = 5000), las = 2, lwd = 3,
